1ac0dc9c0c21d2ec0c0bcb82bc4ba98cfa3c3474
jandevaishnavi <113428453+jandevaishnavi@users.noreply.github.com>
2024-10-14 19:28:12 +0530

    Update main.js

--- main.js
+++ main.js
@@ -10,6 +10,10 @@ var s = document.querySelector('.q1');
 }
 
 */
+
+let n=prompt("enter your name");
+//document.write(n);
+
  function q1() {
             // Get the selected radio button value from the "fruit" group
            var selectedq1 = document.querySelector('input[name="fruit"]:checked');
@@ -21,7 +25,7 @@ var s = document.querySelector('.q1');
             if (selectedq1) {
                 // Compare the selected value with the correct answer
                 if (selectedq1.value === correctanswerq1) {
-                    alert("Congratulations " + correctanswerq1  +" is the correct answer!");
+                    alert("Congratulations" + " "+ n + " " + correctanswerq1  +" is the correct answer!");
                     
      var element1=document.querySelector('.q1');
                     element1.style.visibility='hidden';            
@@ -31,7 +35,7 @@ var s = document.querySelector('.q1');
                     element2.style.visibility='visible';                         
                             
                 } else {
-                    alert("Sorry, wrong answer.");
+                    alert("Sorry " + n + " " + "wrong answer.");
                 }
             } else {
                 alert("Please select an option.");
@@ -50,7 +54,7 @@ var s = document.querySelector('.q1');
             if (selectedq2) {
                 // Compare the selected value with the correct answer
                 if (selectedq2.value === correctanswerq2) {
-                    alert("Congratulations " + correctanswerq2  +" is the correct answer!");
+                    alert("Congratulations" + " " + n + " " + correctanswerq2  +" is the correct answer!");
                     
      var element2 = document.querySelector('.q2');
                     element2.style.visibility='hidden';            
@@ -60,7 +64,7 @@ var s = document.querySelector('.q1');
                     element3.style.visibility='visible';                         
                             
                 } else {
-                    alert("Sorry, wrong answer.");
+                    alert("Sorry " + n + " " + "wrong answer.");
                 }
             } else {
                 alert("Please select an option.");
@@ -82,7 +86,7 @@ var selectedq3 =document.querySelector('input[name="river"]:checked');
             if (selectedq3) {
                 // Compare the selected value with the correct answer
                 if (selectedq3.value === correctanswerq3) {
-                    alert("Congratulations " + correctanswerq3 + " is the correct answer!");
+                    alert("Congratulations" + " " + n + " " + correctanswerq3 + " is the correct answer!");
                     
                var element3=document.querySelector('.q3');
                     element3.style.visibility='hidden';            
@@ -93,7 +97,7 @@ var selectedq3 =document.querySelector('input[name="river"]:checked');
                     
                     
                 } else {
-                    alert("Sorry, wrong answer.");
+                alert("Sorry " + n + " " + "wrong answer.");
                 }
             } else {
                 alert("Please select an option.");
@@ -116,7 +120,7 @@ var selectedq4 =document.querySelector('input[name="mountain"]:checked');
             if (selectedq4) {
                 // Compare the selected value with the correct answer
                 if (selectedq4.value === correctanswerq4) {
-                    alert("Congratulations " + correctanswerq4 + " is the correct answer!");
+                    alert("Congratulations" + " " + n + " " + correctanswerq4 + " is the correct answer!");
                     
                var element4=document.querySelector('.q4');
                     element4.style.visibility='hidden';            
@@ -127,7 +131,7 @@ var selectedq4 =document.querySelector('input[name="mountain"]:checked');
                     
                     
                 } else {
-                    alert("Sorry, wrong answer.");
+                    alert("Sorry " + n + " " + " wrong answer.");
                 }
             } else {
                 alert("Please select an option.");
@@ -147,7 +151,7 @@ var selectedq5 =document.querySelector('input[name="country"]:checked');
             if (selectedq5) {
                 // Compare the selected value with the correct answer
                 if (selectedq5.value === correctanswerq5) {
-                    alert("Congratulations " + correctanswerq5 + " is the correct answer!");
+                    alert("Congratulations" + " " + n + " " + correctanswerq5 + " is the correct answer!");
                     
                        
              var element5=document.querySelector('.q5');
@@ -157,7 +161,7 @@ var selectedq5 =document.querySelector('input[name="country"]:checked');
                     
                     
                 } else {
-                    alert("Sorry, wrong answer.");
+                    alert("Sorry " + n + " " + " wrong answer.");
                 }
             } else {
                 alert("Please select an option.");
